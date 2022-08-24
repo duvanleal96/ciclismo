99e0aa82fa41409b24f0ed9546f2b520951191a6
duvan leal <duvanleal65@gmail.com>
2022-08-24 15:28:36 -0500

    se agrega una prueba unitaria

---
+++ src/test/java/com/sofka/demo/controllerTest/CyclistTest.java
@@ -0,0 +1,31 @@
+package com.sofka.demo.controllerTest;
+
+import com.sofka.demo.repository.CyclistRepository;
+import com.sofka.demo.services.CyclistService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class CyclistTest {
+    @MockBean
+    CyclistRepository cyclistRepository;
+
+    @Autowired
+    CyclistService cyclistService;
+
+    @Test
+    public void testSaveCyclist(){
+       when(cyclistRepository.findAll()).thenReturn(new ArrayList<>());
+        assertThat(cyclistService.getCyclist()).isEmpty();
+        verify(cyclistRepository).findAll();
+    }
+}
+
---
+++ src/test/java/com/sofka/demo/serviceTest/TeamServicesTest.java
@@ -0,0 +1,31 @@
+package com.sofka.demo.serviceTest;
+
+import com.sofka.demo.domain.CountryModel;
+import com.sofka.demo.domain.CyclistModel;
+import com.sofka.demo.domain.TeamModel;
+import com.sofka.demo.services.CyclistService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class TeamServicesTest {
+    @Autowired
+    CyclistService cyclistService;
+
+    @Test
+    public void testSaveCyclist() throws IllegalAccessException {
+        CyclistModel cyclistModel = new CyclistModel(1l,"duvan enrique leal","123",
+                new CountryModel(1l,"colombia",new ArrayList<>(),new ArrayList<>()),
+                new TeamModel(1l,"akastuki","123",new CountryModel(),new ArrayList<>()));
+        CyclistModel cyclistCreated = cyclistService.saveCyclist(cyclistModel);
+        assertNotNull(cyclistCreated);
+    }
+}
